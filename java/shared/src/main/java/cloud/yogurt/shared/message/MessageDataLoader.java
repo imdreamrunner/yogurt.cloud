df0d1b84baa89d0551c810029f3306a64e6f487c
Lu Shengliang <lushl9301@gmail.com>
2016-04-04 20:38:47 +0800

    Update MessageDataLoader.java

--- java/shared/src/main/java/cloud/yogurt/shared/message/MessageDataLoader.java
+++ java/shared/src/main/java/cloud/yogurt/shared/message/MessageDataLoader.java
@@ -1,6 +1,8 @@
 package cloud.yogurt.shared.message;
 
 import java.io.InputStream;
-
+/**
+ * abstrct class that extends InputSteam Class for data streaming
+ */
 public abstract class MessageDataLoader extends InputStream {
 }
